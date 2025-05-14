cf539a36ed4888da25afc2d7d621202246cb43e3
Yuralitv1982 <yuralitv02@gmail.com>
2025-05-14 19:00:26 +0300

    end

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
       duration: 10,
    });
 
+   const inputIsValid = userInput.duration >= 1;
+
    function handleChange(inputIndentifier, newValue) {
       setUserInput((prevUserInput) => {
          return {
@@ -20,11 +22,15 @@ function App() {
          };
       });
    }
+
    return (
       <>
          <Header />;
          <UserInput userInput={userInput} onChange={handleChange} />
-         <Results input={userInput} />
+         {!inputIsValid && (
+            <p className='center'>Please enter a duration greater then zero</p>
+         )}
+         {inputIsValid && <Results input={userInput} />}
       </>
    );
 }
